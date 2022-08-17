6265db34970299de65ff21833294684f63e7336c
unknown <bernasconivargas.mateo@gmail.com>
2022-08-17 18:08:41 -0500

    new link

--- src/App.js
+++ src/App.js
@@ -135,7 +135,7 @@ class App extends Component {
 
   onButtonSubmit = (event) => {
     this.setState({imageUrl: this.state.input})
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://api-smartbrain.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -145,7 +145,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://api-smartbrain.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
