113e48b3b7deb395579422556c4f1df8da0f501d
unknown <bernasconivargas.mateo@gmail.com>
2022-08-17 19:10:37 -0500

    npm start serve

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -38,6 +38,7 @@ class Register extends React.Component {
                 this.props.loadUser(user)
                 this.props.onRouteChange('home')
             }
+            
         }).catch(err => console.log(err))
     }
 
